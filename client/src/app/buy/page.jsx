955f305ff13c84754f2cdc80254f29951781f54e
suryaansh2002 <suryaansh2002@gmail.com>
2024-05-01 18:20:55 +0530

    Buy filter sorting

--- client/src/app/buy/page.jsx
+++ client/src/app/buy/page.jsx
@@ -3,9 +3,11 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import FeaturedCard from '../components/FeaturedCard'
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
-import { Checkbox, Slider, Select } from 'antd'
+import { Checkbox, Slider, Select, Spin } from 'antd'
 import { FaFilter } from 'react-icons/fa'
 import { AmountWithCommas } from '../utils'
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons'
+// import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
 const { Option } = Select
 
@@ -16,9 +18,9 @@ export default function Buy() {
   const [loading, setLoading] = useState(false)
   const [showFilters, setShowFilters] = useState(false)
   const [seatsCount, setSeatsCount] = useState([])
-useEffect(()=>{
-  console.log(filters)
-},[filters])
+  useEffect(() => {
+    console.log(filters)
+  }, [filters])
   const handleBrandChange = (checkedValues) => {
     setFilters({ ...filters, brand: checkedValues })
   }
@@ -96,6 +98,7 @@ useEffect(()=>{
 
   const handleSubmit = async () => {
     console.log(filters)
+    setShowFilters(false)
     let tempFilters = { ...filters }
     if (tempFilters['owners']) {
       if (tempFilters['owners'] == 'Any') {
@@ -117,12 +120,15 @@ useEffect(()=>{
 
   const fetchFilteredListings = async (filters) => {
     try {
+      setLoading(true)
       const response = await axios.post(url + 'api/listings/filtered', filters)
       if (response.data) {
         console.log(response.data)
         setListings(response.data)
       }
+      setLoading(false)
     } catch (e) {
+      setLoading(false)
       console.log(e.message)
     }
   }
@@ -134,9 +140,33 @@ useEffect(()=>{
     fetchAllBrands()
     fetchAllSeats()
   }, [])
-const kmsDrivenRange = [0, 100000]
-const budgetRange = [0, 1500000]
-const yearRange = [2010, new Date().getFullYear()]
+
+  const [selectedOption, setSelectedOption] = useState('')
+  const sortListings = (key, order) => {
+    const tempListings = [...listings]
+    tempListings.sort((a, b) => {
+      if (order === 'asc') {
+        return a[key] - b[key]
+      } else if (order === 'desc') {
+        return b[key] - a[key]
+      }
+      return 0
+    })
+    setListings(tempListings)
+  }
+  const handleSort = (value) => {
+    setSelectedOption(value)
+    console.log(value)
+    if (value.length) {
+      let params = value.split('_')
+      sortListings(params[0], params[1])
+      console.log(params)
+    }
+  }
+
+  const kmsDrivenRange = [0, 100000]
+  const budgetRange = [0, 1500000]
+  const yearRange = [2010, new Date().getFullYear()]
   return (
     <div>
       <div className="text-center">
@@ -159,14 +189,11 @@ const yearRange = [2010, new Date().getFullYear()]
             <div>
               <h3 className="font-bold">Brand:</h3>
               <Checkbox.Group
-              defaultValue={filters['brand']}
-              onChange={handleBrandChange}>
+                defaultValue={filters['brand']}
+                onChange={handleBrandChange}
+              >
                 {brands.map((brand) => (
-                  <Checkbox
-                    value={brand}
-                  >
-                    {brand}
-                  </Checkbox>
+                  <Checkbox value={brand}>{brand}</Checkbox>
                 ))}
               </Checkbox.Group>
             </div>
@@ -204,9 +231,10 @@ const yearRange = [2010, new Date().getFullYear()]
             {/* Fuel Type */}
             <div>
               <h3 className="font-bold">Fuel Type:</h3>
-              <Checkbox.Group onChange={handleFuelTypeChange}               
-              defaultValue={filters['fuelType']}
->
+              <Checkbox.Group
+                onChange={handleFuelTypeChange}
+                defaultValue={filters['fuelType']}
+              >
                 <Checkbox value="Petrol">Petrol</Checkbox>
                 <Checkbox value="Diesel">Diesel</Checkbox>
                 <Checkbox value="CNG">CNG</Checkbox>
@@ -216,9 +244,10 @@ const yearRange = [2010, new Date().getFullYear()]
             {/* Transmission Type */}
             <div>
               <h3 className="font-bold">Transmission Type:</h3>
-              <Checkbox.Group onChange={handleTransmissionTypeChange}
-                            defaultValue={filters['transmissionType']}
-                            >
+              <Checkbox.Group
+                onChange={handleTransmissionTypeChange}
+                defaultValue={filters['transmissionType']}
+              >
                 <Checkbox value="automatic">Automatic</Checkbox>
                 <Checkbox value="manual">Manual</Checkbox>
               </Checkbox.Group>
@@ -227,7 +256,10 @@ const yearRange = [2010, new Date().getFullYear()]
             {/* Owners */}
             <div>
               <h3 className="font-bold">Owners:</h3>
-              <Select defaultValue={filters['owners'] ? filters['owners'] : 'Any'} onChange={handleOwnersChange}>
+              <Select
+                defaultValue={filters['owners'] ? filters['owners'] : 'Any'}
+                onChange={handleOwnersChange}
+              >
                 <Option value="Any">Any</Option>
                 <Option value="1">1</Option>
                 <Option value="2">2</Option>
@@ -256,7 +288,10 @@ const yearRange = [2010, new Date().getFullYear()]
             {/* No. of Seats */}
             <div>
               <h3 className="font-bold">No. of Seats:</h3>
-              <Checkbox.Group defaultValue={filters['seats']} onChange={handleSeatsChange}>
+              <Checkbox.Group
+                defaultValue={filters['seats']}
+                onChange={handleSeatsChange}
+              >
                 {seatsCount.map((count) => (
                   <Checkbox value={count}>{count}</Checkbox>
                 ))}
@@ -271,12 +306,46 @@ const yearRange = [2010, new Date().getFullYear()]
           </div>
         </div>
       )}
+      <div className="text-center mt-8">
+        <Select
+          className="border border-gray-300 ml-4 text-lg md:ml-12 rounded px-16 w-[12rem] py-8 mt-4 md:translate-x-6 translate-x-2"
+          value={selectedOption}
+          onChange={handleSort}
+        >
+          <Option value="">
+            <span>Sort By...</span>
+            {/* <ArrowUpOutlined /> */}
+          </Option>
+          <Option value="price_asc">
+            <span>Price: Low to High</span>
+          </Option>
+          <Option value="price_desc">
+            <span>Price: High to Low</span>
+          </Option>
+          <Option value="kmDriven_asc">
+            <span>KM Driven: Low to High</span>
+          </Option>
+          <Option value="kmDriven_desc">
+            <span>KM Driven: High to Low</span>
+          </Option>
+          <Option value="year_asc">
+            <span>Year: Low to High</span>
+          </Option>
+          <Option value="year_desc">
+            <span>Year: High to Low</span>
+          </Option>
+        </Select>
+      </div>
       {loading ? (
-        <div>Loading...</div>
+        <div className="text-center mt-8 mb-12">
+          <Spin size="large" />
+        </div>
       ) : listings.length == 0 ? (
-        <div className='text-lg font-semibold text-left ml-8 h-[70vh] p-4'>No vehicles match these filters</div>
+        <div className="text-lg font-semibold text-left ml-8 h-[70vh] p-4">
+          No vehicles match these filters
+        </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-8 px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-8 px-8 mb-12">
           {listings.map((car) => (
             <FeaturedCard key={car.id} car={car} />
           ))}

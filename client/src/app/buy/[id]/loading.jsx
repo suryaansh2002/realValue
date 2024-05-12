8c494db9392ee35d08503db702dc127d74aff558
Chirag <chirag@skylarkdrones.com>
2024-05-12 10:51:42 +0530

    Small fix

---
+++ client/src/app/buy/[id]/loading.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import { Oval } from 'react-loader-spinner'
+
+const loading = () => {
+  return (
+    <div className="flex items-center justify-center p-2 h-[100vh]">
+      {/* Style this loading in the center of screen */}
+      <Oval color="#000" height={50} width={50} />
+    </div>
+  )
+}
+
+export default loading
